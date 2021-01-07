33657a8a8bbe2c5fe382de238a48391e417be46c
cedouiri <cedouiri@gmail.com>
2021-01-07 22:42:21 +0000

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+'''
+a method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file
+'''
+
+def minOperations(n):
+    '''
+    min operations
+    '''
+    count = 0
+    i = 2
+    while n > 1:
+        while n % i == 0:
+            count = count + i
+            n = n // i
+        i = i + 1
+    return count
+
+
