641bbe16f71c9ba00106cde05d84177f3f7293d5
cedouiri <cedouiri@gmail.com>
2021-01-14 17:43:28 +0000

    HEADER

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef HEADER
+#define HEADER
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void topple(int grid[3][3]);
+int is_table(int grid[3][3]);
+void cp_grid(int grid[3][3], int new_grid[3][3]);
+
+#endif
